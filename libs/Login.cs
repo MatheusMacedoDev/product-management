8140262bf251c8fd6c8cfdc54348ca66435dbcff
Matheus Macedo Santos <matheus.macedo.developer@gmail.com>
2023-05-16 10:58:10 -0300

    Creating basic Login

--- libs/Login.cs
+++ libs/Login.cs
@@ -1,7 +1,41 @@
 namespace ProductManagement.libs 
 {
 	public class Login
-    {
-    	
-    }
+	{
+		public bool IsLoggedIn { get; private set; }
+		
+		public Login()
+		{
+			
+		}
+		
+		public string turnLoggedIn(string email, string password) 
+		{
+			List<User> users = User.List();
+			User findedUser = users.Find(user => user.Email == email);
+			
+			if (findedUser != null) 
+			{
+				if (findedUser.TryPassword(password)) 
+				{
+					return $"The user has been logged in!";
+				}
+				else 
+				{
+					return $"Incorrect password!";
+				}
+			}
+			else 
+			{
+				return "There is no user with this email!";
+			}
+			
+		}
+		
+		public string turnLoggedOut() 
+		{
+			IsLoggedIn = false;
+			return $"The user has been logged out!";
+		}
+	}
 }
