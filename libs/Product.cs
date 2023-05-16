109e9b798e7efc29e25d830fe25df26dbbc95c7b
Matheus Macedo Santos <matheus.macedo.developer@gmail.com>
2023-05-16 10:45:15 -0300

    Creating basic Product

--- libs/Product.cs
+++ libs/Product.cs
@@ -1,7 +1,41 @@
 namespace ProductManagement.libs 
 {
 	public class Product
-    {
-    	
-    }
+	{
+		public int Id { get; private set; }
+		public string Name { get; private set; }
+		public float Price { get; private set; }
+		public DateTime RegistrationDate { get; private set; }
+		public Brand Brand { get; private set; }
+		public User UserWhoRegistered { get; private set; }
+		
+		private static LinkedList<Product> products = new LinkedList<Product>();
+
+		public Product(int id, string name, float price, Brand brand, User userWhoRegistered) 
+		{
+			this.Id = id;
+			this.Name = name;
+			this.Price = price;
+			this.RegistrationDate = DateTime.Now;
+			this.Brand = brand;
+			this.UserWhoRegistered = userWhoRegistered;
+		}
+		
+		public static string Registrate(Product product) 
+		{
+			products.AddLast(product);
+			return $"The brand ${product.Name} has been registrated!";
+		} 
+		
+		public static List<Product> List() 
+		{
+			return products.ToList();
+		}
+		
+		public string Delete(Product product) 
+		{
+			products.Remove(product);
+			return $"The brand ${product.Name} has been deleted!";
+		}
+	}
 }
