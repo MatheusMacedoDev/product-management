65681f189f3e0b8efcc4ad81782dbd50d54f47cb
Matheus Macedo Santos <matheus.macedo.developer@gmail.com>
2023-05-16 10:17:41 -0300

    Creating basic User

--- libs/User.cs
+++ libs/User.cs
@@ -1,7 +1,39 @@
 namespace ProductManagement.libs 
 {
 	public class User
-    {
-    	
-    }
+	{
+		public int Id { get; private set; }
+		public string Name { get; private set; }
+		public string Email { get; private set; }
+		public string Password { get; private set; }
+		public DateTime RegistrationDate { get; private set; }
+
+		private static LinkedList<User> users = new LinkedList<User>();
+		
+		public User(int id, string name, string email, string password) 
+		{
+			this.Id = id;
+			this.Name = name;
+			this.Email = email;
+			this.Password = password;
+			this.RegistrationDate = DateTime.Now;
+		}
+		
+		public static string Register(User user) 
+		{
+			users.AddLast(user);
+			return $"The user ${user.Name} has been registrated!";
+		}
+		
+		public static List<User> List() 
+		{
+			return users.ToList();
+		}
+		
+		public string Delete(User user) 
+		{
+			users.Remove(user);
+			return $"The user ${user.Name} has been deleted!";
+		}
+	}
 }
