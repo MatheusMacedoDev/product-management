4377af5089ddbc0257014379919867b50ad93c34
Matheus Macedo Santos <matheus.macedo.developer@gmail.com>
2023-05-17 09:28:09 -0300

    Making menu with the user functionalities

--- libs/Application.cs
+++ libs/Application.cs
@@ -11,12 +11,46 @@ namespace ProductManagement.libs
 		
 		public Application()
 		{
-			User.Register(new User(0, "Matheus Macedo", "matheus@email.com", "123456"));
+			User.Register(new User("Matheus Macedo", "matheus@email.com", "123456"));
 			
 			bool systemOpened = loginArea();
 			if (systemOpened) 
 			{
-				Console.WriteLine($"The system has been opened!");
+				byte option = 0;
+				do 
+				{
+					Console.Clear();
+					Console.WriteLine($"Management options");
+					Console.WriteLine($"(1) Register a new user");
+					Console.WriteLine($"(2) List users");
+					Console.WriteLine($"(3) Delete some user");
+					Console.WriteLine($"(4) Exit");
+					Console.WriteLine($"");
+					
+					Console.Write($"Type here: ");
+					option = byte.Parse(Console.ReadLine()); 
+					
+					switch(option) 
+					{
+						case 1:
+							registerNewUserArea();
+							break;
+						case 2:
+							listUsersArea();
+							break;
+						case 3:
+							deleteUserArea();
+							break;
+						case 4:
+							break;
+						default:
+							Console.WriteLine($"Invalid option!");
+							break;
+					}
+					
+					waitForAnyButton();
+				}
+				while (option != 4);
 			}
 		}
 		
@@ -34,6 +68,57 @@ namespace ProductManagement.libs
 			Console.WriteLine(loginResponse);
 			
 			return login.IsLoggedIn;
+		}
+		
+		private void registerNewUserArea() 
+		{
+			Console.Clear();
+			
+			Console.WriteLine($"Type the user information");
+
+			Console.Write($"Name: ");
+			string name = Console.ReadLine();
+
+			Console.Write($"E-mail: ");
+			string email = Console.ReadLine();
+
+			Console.Write($"Password: ");
+			string password = Console.ReadLine();
+			
+			User newUser = new User(name, email, password);
+			User.Register(newUser);
+		}
+		
+		private void listUsersArea() 
+		{
+			Console.Clear();
+			
+			Console.WriteLine($"");
+			Console.WriteLine($"List of users");
+			
+			List<User> users = User.List();
+			
+			foreach (User user in users) 
+			{
+				Console.WriteLine($"{user.Id}: {user.Name} <{user.Email}>");
+			}
+		}
+		
+		private void deleteUserArea() 
+		{
+			Console.Clear();
+			
+			Console.WriteLine($"Delete user");
+			Console.Write($"Type the user id: ");
+			byte id = byte.Parse(Console.ReadLine());
+			User.Delete(id);
+		}
+		
+		private void waitForAnyButton() 
+		{
+			Console.WriteLine($"");
+			Console.WriteLine($"Type any key to continue...");
+			Console.ReadLine();
 		}
 	}
 }
